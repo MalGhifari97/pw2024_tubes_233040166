3d23e3a5af162cad80ab225b2532378e68476987
Mal_Ghifari97 <145556126+MalGhifari97@users.noreply.github.com>
2024-06-11 16:45:02 +0700

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,13 @@
-<footer id="footer">
+<footer id="footer" >
     
     
-    <div class="copyright text-center">
-        &copy;copyright <strong><span>Mekar Laksana</span></strong>. All Rights Reserved
-        <div class="credite">
-            Designed By <a href="https://malghifari97.github.io/">Mal_Ghifari97</a>
-        </div>
+    <div id="po" class="copyright text-center" style="padding:10px;margin-top: 0;">
+    <div class="icons text-center">
+        <a href="https://www.facebook.com/profile.php?id=100017635476187"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/mal_ghifari97/"><i class="bx bxl-instagram"></i></a>
+        <a href="https://github.com/MalGhifari97"><i class='bx bxl-github'></i></a>
+        <a href="https://www.linkedin.com/in/muhammad-akmal-al-ghifari-72a97b297/"><i class='bx bxl-linkedin'></i></a>
+    </div>
+        &copy; 2024 - Made By Mal_Ghifari97 | All Right Reserved.
     </div>
 </footer>
